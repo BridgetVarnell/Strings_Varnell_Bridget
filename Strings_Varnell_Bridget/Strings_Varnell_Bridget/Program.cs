195054e80db83369715a8a061060f2fbc3836857
Bridget Varnell <bridget.varnell24@gmail.com>
2026-03-02 08:36:11 -0600

    Workk

--- Strings_Varnell_Bridget/Strings_Varnell_Bridget/Program.cs
+++ Strings_Varnell_Bridget/Strings_Varnell_Bridget/Program.cs
@@ -1,5 +1,36 @@
 ﻿using System
+﻿using System;
 
+namespace Strings_Varnell_Bridget
+{
     class Program
 { 
-static void}
+static void}    {
+        static void Main(string[] args)
+        {
+            // Askes user to input a character and assigns input to a variable
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks if the character user entered is a letter
+            bool isLetter = Char.IsLetter(myChar);
+
+            // Prints the results to the console
+            Console.WriteLine($"\n{myChar} is letter: {isLetter}.");
+
+            // Asks user a qusetion and saves response to a variable, sentence
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            // Asks user to enter a word, saves response to a variable called word
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            // Checks if the word is in the sentence
+            bool isIn = sentence.Contains(word);
+
+            // Prints if the word is in the sentence
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
+        }
+    }
+}
